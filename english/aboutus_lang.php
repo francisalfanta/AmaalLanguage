1fd98167c788e205e755c91a0f6a2a6f01745e49
francisalfanta <francisalfanta@gmail.com>
2015-12-09 11:16:08 +0400

    Amaal Languange English

---
+++ english/aboutus_lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['aboutus_title'] 		   = 'About Us';
+$lang['aboutus_content'] 	   = "Amaal is one of the powerful and smart product by Hosting Castle. 
+Today's generation most of consumers rely on internet to get the service they need to ease travel time and effort.Amaal helps you to make your services available online. From custom designs and vast collection of themes, you can pick which look will best fit your business. With Amaal you can monitor and control your business anytime and anywhere you are.You can post great deals and promote future services to boost your market. And with the help of our in house team we can build features and solutions you need.";
+
+$lang['aboutus_sidebar_menu_title'] = 'Our Company';
+$lang['aboutus_sidebar_menu1'] = 'About Aqary';
+$lang['aboutus_sidebar_menu1'] = 'Our Team';
+$lang['aboutus_sidebar_menu1'] = 'How We Work';
+$lang['aboutus_sidebar_menu1'] = 'Latest News';
+$lang['aboutus_sidebar_menu1'] = 'Careers';
---
+++ english/latestnews_lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['latestnews_title'] 		 = 'Latest News';
+$lang['latestnews_header_title'] = 'Amaal Professional Service';
+$lang['latestnews_content'] 	 = "We're opening for free trial.";
